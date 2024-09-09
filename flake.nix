f0956ed6dcc3906c5998de86af932ad37cdf5dcd
Locochoco <ivanrwpf@gmail.com>
2024-09-09 16:44:38 -0300

    added cuda cachix

--- flake.nix
+++ flake.nix
@@ -27,4 +27,10 @@
       }) // {
         overlay.visual-slam = import ./overlay.nix;
       };
+
+      
+  nixConfig = {
+    extra-substituters = [ "https://cuda-maintainers.cachix.org" ];
+    extra-trusted-public-keys = [ "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E=" ];
+  };
 }
