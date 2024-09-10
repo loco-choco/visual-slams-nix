9dd53ded883633147e34705650d59811fad08843
Locochoco <ivanrwpf@gmail.com>
2024-09-10 01:00:38 -0300

    formatting

--- flake.nix
+++ flake.nix
@@ -28,9 +28,10 @@
         overlay.visual-slam = import ./overlay.nix;
       };
 
-      
   nixConfig = {
     extra-substituters = [ "https://cuda-maintainers.cachix.org" ];
-    extra-trusted-public-keys = [ "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E=" ];
+    extra-trusted-public-keys = [
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+    ];
   };
 }
